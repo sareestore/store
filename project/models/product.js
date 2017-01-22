f6d9ee2c56911c52a8fcc5838307e154b24b7c10
nagasudhirpulla <nagasudhirpulla@gmail.com>
2017-01-22 13:10:10 +0530

    products get clause modified

--- project/models/product.js
+++ project/models/product.js
@@ -4,8 +4,11 @@ var tableName = "products";
 var tableColumns = ["id", "description", "price", "size"];
 
 exports.get = function (done) {
-    var sql = "SELECT products.id, products.size, products.description, products.price, GROUP_CONCAT(DISTINCT products_colors.colors_id SEPARATOR ', ') AS color_ids, GROUP_CONCAT(DISTINCT products_images.image_url SEPARATOR ', ') AS image_urls, GROUP_CONCAT(DISTINCT products_occasions.occasions_id SEPARATOR ', ') AS occasions_ids, GROUP_CONCAT(DISTINCT products_tags.tags_id SEPARATOR ', ') AS tags_ids, GROUP_CONCAT(DISTINCT products_types.product_types_id SEPARATOR ', ') AS product_types_ids FROM products LEFT OUTER JOIN products_colors ON products_colors.products_id = products.id LEFT OUTER JOIN products_tags ON products_tags.products_id = products.id LEFT OUTER JOIN products_images ON products_images.products_id = products.id LEFT OUTER JOIN products_occasions ON products_occasions.products_id = products.id LEFT OUTER JOIN products_types ON products_types.products_id = products.id GROUP BY products.id";
+    var sql = "SELECT products.id, products.size, products.description, products.price, GROUP_CONCAT(DISTINCT products_colors.colors_id SEPARATOR ', ') AS color_ids, GROUP_CONCAT(DISTINCT products_images.image_url SEPARATOR ', ') AS image_urls, GROUP_CONCAT(DISTINCT products_occasions.occasions_id SEPARATOR ', ') AS occasions_ids, GROUP_CONCAT(DISTINCT products_tags.tags_id SEPARATOR ', ') AS tags_ids, GROUP_CONCAT(DISTINCT products_types.product_types_id SEPARATOR ', ') AS product_types_ids FROM products LEFT OUTER JOIN products_colors ON products_colors.products_id = products.id LEFT OUTER JOIN products_tags ON products_tags.products_id = products.id LEFT OUTER JOIN products_images ON products_images.products_id = products.id LEFT OUTER JOIN products_occasions ON products_occasions.products_id = products.id LEFT OUTER JOIN products_types ON products_types.products_id = products.id";
+    // Add the group by clause
+    sql += " GROUP BY products.id";
     sql += ";";
+    //select products.id FROM products LEFT OUTER JOIN products_colors WHERE products_colors.colors_id = 1
     db.get().query(sql, [], function (err, rows) {
         if (err) return done(err);
         // console.log("result of product images fetch is " + JSON.stringify(result));
